3e32472d2f97ad9f066d2c787d1aa3f52d609f53
zahrahosseini99 <hosseini99.zahra@gmail.com>
2019-10-12 09:54:46 +0330

    A3

---
+++ A3/A3/Q1MergeSort.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestCommon;
+
+namespace A3
+{
+    public class Q1MergeSort : Processor
+    {
+        public Q1MergeSort(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long[]>)Solve);
+
+        public long[] Solve(long n, long[] a)
+        {
+            if (n <= 1)
+                return a;
+            MergeSort(a, 0, n - 1);
+            return a;
+        }
+
+        public static void MergeSort(long[] array, long low, long high)
+        {
+            if (low < high)
+            {
+                long mid = ((low + high) / 2);
+                MergeSort(array, low, mid);
+                MergeSort(array, mid + 1, high);
+                Merge(array, low, mid, high);
+            }
+        }
+
+
+        private static void Merge(long[] array, long low, long mid, long high)
+        {
+
+            long left = low;
+            long right = mid + 1;
+            long[] arr = new long[(high - low) + 1];
+            long index = 0;
+
+            while ((left <= mid) && (right <= high))
+            {
+                if (array[left] < array[right])
+                {
+                    arr[index] = array[left];
+                    left = left + 1;
+                }
+                else
+                {
+                    arr[index] = array[right];
+                    right = right + 1;
+                }
+                index = index + 1;
+            }
+
+            if (left <= mid)
+            {
+                while (left <= mid)
+                {
+                    arr[index] = array[left];
+                    left = left + 1;
+                    index = index + 1;
+                }
+            }
+
+            if (right <= high)
+            {
+                while (right <= high)
+                {
+                    arr[index] = array[right];
+                    right = right + 1;
+                    index = index + 1;
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                array[low + i] = arr[i];
+            }
+
+        }
+
+
+    }
+}
---
+++ A3/A3/Q2FibonacciFast.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A3
+{
+    public class Q2FibonacciFast : Processor
+    {
+        public Q2FibonacciFast(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long>)Solve);
+
+        public long Solve(long n)
+        {
+            
+            if (n <= 1)
+                return n;
+            long[] fib = new long[n];
+            fib[0] = 1;
+            fib[1] = 1;
+         
+            for(int i =2; i < n; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+            return fib[n-1];
+        }
+    }
+}
