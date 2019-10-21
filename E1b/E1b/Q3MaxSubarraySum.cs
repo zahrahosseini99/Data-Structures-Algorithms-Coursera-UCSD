bb50f084f11d2ac9bc8a17e29bad1377608555e2
zahrahosseini99 <hosseini99.zahra@gmail.com>
2019-10-21 14:58:18 +0330

    Exam1b

---
+++ E1b/E1b/Q3MaxSubarraySum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+using System.Linq;
+namespace E1b
+{
+    public class Q3MaxSubarraySum : Processor
+    {
+        public Q3MaxSubarraySum(string testDataName) : base(testDataName)
+        { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long>)Solve);
+
+
+        public virtual long Solve(long n, long[] numbers)
+        {
+            //long sum = 0;
+            //List<Tuple<long, int>> nums = new List<Tuple<long, int>>();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    nums.Add(Tuple.Create(numbers[i],i));
+            //}
+          long m=  numbers.ToList().Max();
+            for (int i = 0; i < n; i++)
+            {
+                if (m == numbers[i])
+                    m += numbers[i + 1];
+            }
+            return m;
+        }
+        //public long Maximum(long[] numbers)
+        //{
+        //    long max = numbers[0];
+        //    foreach (var c in numbers)
+        //    {
+        //        if (max + c > max)
+        //            max = max + c;
+        //    }
+        //    return max;
+        //}
+
+        //private long Check(long max, long c)
+        //{
+        //    if (max + c > max)
+        //        max = max + c;
+        //    return max;
+        //}
+    }
+}
---
+++ E1b/E1b/Q4HungryFrog.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using TestCommon;
+
+namespace E1b
+{
+    public class Q4HungryFrog : Processor
+    {
+        public Q4HungryFrog(string testDataName) : base(testDataName)
+        { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long, long[][], long>)Solve);
+
+
+        public virtual long Solve(long n, long p, long[][] numbers)
+        {
+            long sum = 0;
+            int index = -1;
+            if ((numbers[0][0] >= numbers[1][0]))
+            {
+                sum += numbers[0][0];
+                index = 0;
+            }
+
+            else
+            {
+                sum += numbers[1][0];
+                index = 1;
+
+            }
+                
+            for (int i = 1; i < n; i++)
+            {
+                if (numbers[0][i] >= numbers[1][i])
+                {
+                    
+                    if (index == 1)
+                    {
+                        sum += (numbers[0][i] - p);
+                    }
+                    else
+                    {
+                        sum += (numbers[0][i]);
+                        index = 0;
+                    }
+
+                }
+
+                else
+
+                {
+                   
+                    if (index == 0)
+                    {
+                        sum += (numbers[1][i]-p);
+                    }
+                    else
+                    {
+                        sum += (numbers[1][i]);
+                        index = 1;
+                    }
+                  
+                }
+                   
+            }
+            return sum;
+        }
+    }
+}
