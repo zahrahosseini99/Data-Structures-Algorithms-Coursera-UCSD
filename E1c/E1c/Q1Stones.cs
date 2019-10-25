bfb5fc839798b01d7307279c536470967bcbae93
zahrahosseini99 <hosseini99.zahra@gmail.com>
2019-10-25 22:29:00 +0330

    Exam1c

---
+++ E1c/E1c/Q1Stones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TestCommon;
+
+namespace E1c
+{
+    public class Q1Stones : Processor
+    {
+        public Q1Stones(string testDataName) : base(testDataName)
+        { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long>)Solve);
+
+        //public virtual long Solve(long n, long[] stones)
+        //{
+
+        //    int i = 0;
+        //    long sum = 0;
+        //    for (i = 0; i < stones.Length; i++)
+        //    {
+
+        //        sum+= stones[i];
+        //        if (sum >= n)
+        //       break;
+        //    }
+        //    if (n > sum)
+        //        return 0;
+        //    return i + 1;
+        //}
+        public virtual long Solve(long n, long[] stones)
+        {
+            long sum = 0;
+            int i = 0;
+            while (i < stones.Length)
+            {
+                sum += stones[i];
+                if (sum >= n)
+                    break;
+                i++;
+            }
+            if (n > sum)
+                return 0;
+            return i + 1;
+        }
+    }
+}
---
+++ E1c/E1c/Q4HungryFrog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TestCommon;
+using System.Linq;
+namespace E1c
+{
+    public class Q4HungryFrog : Processor
+    {
+        public Q4HungryFrog(string testDataName) : base(testDataName)
+        { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long, long[][], long>)Solve);
+        public virtual long Solve(long n, long p, long[][] numbers)
+        {
+            long[] first = new long[n];
+            long[] second = new long[n];
+            first[0] = numbers[0][0];
+            second[0] = numbers[1][0];
+            for (int i = 1; i < n; i++)
+            {
+                if ((first[i - 1] + numbers[0][i]) > (second[i - 1] + numbers[0][i]-p))
+                    first[i] = (first[i - 1] + numbers[0][i]);
+                else
+                    first[i] = (second[i - 1] + numbers[0][i]-p);
+                if ((second[i - 1] + numbers[1][i]) > (first[i - 1] + numbers[1][i] - p))
+                    second[i] = (second[i - 1] + numbers[1][i]);
+                else
+                    second[i] = (first[i - 1] + numbers[1][i] - p);
+            }
+            if (second[n - 1] > first[n - 1])
+                return second[n - 1];
+            else
+                return first[n - 1];
+        }
+
+       
+    }
+}
