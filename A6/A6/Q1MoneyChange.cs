6da0cd7251389a42febac2a8c8d9677a92a19ea7
zahrahosseini99 <hosseini99.zahra@gmail.com>
2019-11-09 17:08:43 +0330

    A6

---
+++ A6/A6/Q1MoneyChange.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A6
+{
+    public class Q1MoneyChange : Processor
+    {
+        private static readonly int[] COINS = new int[] { 1, 3, 4 };
+
+        public Q1MoneyChange(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long>)Solve);
+
+        public long Solve(long n)
+        {
+            long[] coins = { 1, 3, 4 };
+            return Counter(n, coins);
+        }
+
+        private long Counter(long n, long[] coins)
+        {
+            long[] mincoins = new long[n+1];
+            mincoins[0] = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                long count = 0;
+                mincoins[i] = int.MaxValue;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i >= coins[j])
+                        count = mincoins[i - coins[j]] + 1;
+                    if (count <= mincoins[i])
+                        mincoins[i] = count;
+                }
+            }
+            return mincoins[n];
+        }
+    }
+}
---
+++ A6/A6/Q4LCSOfTwo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCommon;
+
+namespace A6
+{
+    public class Q4LCSOfTwo : Processor
+    {
+        public Q4LCSOfTwo(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long[], long[], long>)Solve);
+
+        public long Solve(long[] seq1, long[] seq2)
+        {
+            return LCS2(seq1, seq2 );
+        }
+        static long LCS2(long[] seq1, long[] seq2)
+        {
+            long m = seq1.Length;
+            long n = seq2.Length;
+            int[,] Tabel = new int[m + 1, n + 1];
+
+
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i == 0)
+                        Tabel[i, j] = 0;
+                    else if (j == 0)
+                        Tabel[i, j] = 0;
+                    else if (seq1[i - 1] == seq2[j - 1])
+                        Tabel[i, j] = Tabel[i - 1, j - 1] + 1;
+                    else
+                        Tabel[i, j] = Math.Max(Tabel[i - 1, j], Tabel[i, j - 1]);
+                }
+            }
+            return Tabel[m, n];
+        }
+
+
+    }
+}
