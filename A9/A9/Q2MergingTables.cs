cff743ee5d90836b8617f0c9eb9a094a6d174a54
zahrahosseini99 <hosseini99.zahra@gmail.com>
2019-12-07 18:57:50 +0330

    A9

---
+++ A9/A9/Q2MergingTables.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestCommon;
+
+namespace A9
+{
+    public class Tables
+    {
+        public long[] rank;
+        public long[] parent;
+
+        public long[] tsize;
+        int n;
+        public Tables(long[] t, long[] p,long n)
+        {
+            rank = new long[n];
+            parent = p;
+            tsize = t;
+        }
+
+
+        public long find(long x)
+        {
+
+            if (parent[x] != x)
+            {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+       
+        public long Union(long dest, long source)
+        {
+            long res = tsize.Max();
+            long realdest = find(dest);
+            long realsource = find(source);
+            if (realdest == realsource)
+            {
+                if (res == 0)
+                    return tsize.Max();
+                return res;
+            }
+
+            if (rank[realsource] > rank[realdest])
+            {
+
+                parent[realsource] = realdest;
+                tsize[realdest] = tsize[realsource] + tsize[realdest];
+                tsize[realsource] = 0;
+                res = Math.Max(res, tsize[realdest]) ;
+            }
+            else
+            {
+                parent[realdest] = realsource;
+                tsize[realsource] = tsize[realsource] + tsize[realdest];
+                tsize[realdest] = 0;
+                res = Math.Max(res, tsize[realsource]);
+                if (rank[realdest] == rank[realsource])
+                    rank[realsource] += 1;
+            }
+            return res;
+        }
+
+    }
+    public class Q2MergingTables : Processor
+    {
+
+        public Q2MergingTables(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long[], long[], long[], long[]>)Solve);
+
+
+        public long[] Solve(long[] tableSizes, long[] targetTables, long[] sourceTables)
+        {
+            long m = targetTables.Length;
+            long[] parent = new long[tableSizes.Length + 1];
+           
+            for (int i = 0; i <= tableSizes.Length; i++)
+            {
+
+                parent[i] = i;
+            }
+            
+            Tables example = new Tables(tableSizes, parent, tableSizes.Length);
+
+            long[] res = new long[m];
+            for (int i = 0; i < m; i++)
+            {
+               long a = example.Union(targetTables[i] - 1, sourceTables[i] - 1);
+                if (i != 0)
+                {
+                    res[i] = Math.Max(res[i - 1], a);
+                }
+                else
+                    res[i] = a;
+            }
+
+            return res;
+        }
+
+    }
+}
---
+++ A9/A9/Q4ParallelProcessing.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A9
+{
+    public class Q4ParallelProcessing : Processor
+    {
+        public Q4ParallelProcessing(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], Tuple<long, long>[]>)Solve);
+
+        public Tuple<long, long>[] Solve(long threadCount, long[] jobDuration)
+        {
+            priorityQueue example = new priorityQueue(threadCount);
+            Tuple<long, long>[] res = new Tuple<long, long>[jobDuration.Length];
+            for (long i = 0; i < jobDuration.Length; i++)
+            {
+                res[i] = new Tuple<long, long>(example.Threads[0].Item1, example.Threads[0].Item2);
+                example.ChangePriority(0, (example.Threads[0].Item1
+                    , example.Threads[0].Item2 + jobDuration[i]));
+
+            }
+            return res;
+        }
+
+        public class priorityQueue
+        {
+
+            public long MaxSize;
+            public (long, long)[] Threads;
+            public priorityQueue(long maxsize)
+            {
+                MaxSize = maxsize;
+                Threads = new (long, long)[maxsize];
+                for (long i = 0; i < maxsize; i++)
+                {
+                    Threads[i] = (i, (long)0);
+                }
+
+            }
+            public long Parent(long index) => (index - 1) / 2;
+            public long LeftChild(long index) => index * 2 + 1;
+            public long RightChild(long index) => index * 2 + 2;
+            public void SiftUp(long index)
+            {
+                while (index > 0 && ((Threads[index].Item2 < Threads[Parent(index)].Item2)
+                    || ((Threads[index].Item2 == Threads[Parent(index)].Item2)
+                    && (Threads[index].Item1 < Threads[Parent(index)].Item1))))
+
+                {
+                    Swap(index, Parent(index));
+                    index = Parent(index);
+                }
+
+            }
+            public void SiftDown(long index)
+            {
+                long max = index;
+                long l = LeftChild(index);
+                if (
+                    (l < MaxSize)
+                    &&
+                    (
+                    (Threads[l].Item2 < Threads[max].Item2)
+                    ||
+                    (
+                    (Threads[l].Item2 == Threads[max].Item2)
+                    && (Threads[l].Item1 < Threads[max].Item1)
+                    )
+                    )
+                    )
+                {
+                    max = l;
+
+
+                }
+                long r = RightChild(index);
+                if (
+                    (r < MaxSize)
+                   &&
+                   (
+                   (Threads[r].Item2 < Threads[max].Item2)
+                   ||
+                   (
+                   (Threads[r].Item2 == Threads[max].Item2)
+                   && (Threads[r].Item1 < Threads[max].Item1)
+                   )
+                   )
+                   )
+
+                    max = r;
+                if (index != max)
+                {
+                    Swap(index, max);
+                    SiftDown(max);
+                }
+
+            }
+            public void ChangePriority(long index, (long, long) priority)
+            {
+                (long, long) oldp = Threads[index];
+                Threads[index] = priority;
+                if ((priority.Item2 < oldp.Item2) || ((priority.Item2 == oldp.Item2)
+                    && (priority.Item1 < oldp.Item1)))
+
+                {
+                    SiftUp(index);
+                }
+                if ((priority.Item2 > oldp.Item2) || ((priority.Item2 == oldp.Item2)
+                    && (priority.Item1 > oldp.Item1)))
+                    SiftDown(index);
+            }
+            private void Swap(long a, long b)
+            {
+                var tmp = Threads[a];
+                Threads[a] = Threads[b];
+                Threads[b] = tmp;
+            }
+        }
+
+    }
+
+}
